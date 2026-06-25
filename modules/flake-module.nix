25e82d9ebc821bf65bba37ea255976693c659001
matthis-k <matthis.kaelble@gmail.com>
2026-06-25 15:24:19 +0200

    fix: overlay-only flakeModule

--- modules/flake-module.nix
+++ modules/flake-module.nix
@@ -1,23 +1,7 @@
-{ inputs, ... }: {
-  perSystem = { pkgs, system, ... }: {
+{ ... }: {
+  perSystem = { ... }: {
     phenix.overlays = [(final: prev: {
-      phenix = (prev.phenix or {}) // {
-        hyprland = inputs.hyprland.packages.${system}.hyprland;
-        newshell = pkgs.quickshell;
-        kitty = pkgs.kitty;
-        wayland-utils = pkgs.symlinkJoin {
-          name = "wayland-utils";
-          paths = with pkgs; [
-            grim
-            slurp
-            swappy
-            wl-clipboard
-            tesseract
-          ];
-        };
-        fish = pkgs.fish;
-        starship = pkgs.starship;
-      };
+      phenix = (prev.phenix or {}) // {};
     })];
   };
 }
