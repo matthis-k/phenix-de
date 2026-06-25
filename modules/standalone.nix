659b9654584d345697463363b1c1058321312a34
matthis-k <matthis.kaelble@gmail.com>
2026-06-25 15:30:32 +0200

    rename newshell to phenix-shell

--- modules/standalone.nix
+++ modules/standalone.nix
@@ -1,9 +1,13 @@
 { inputs, ... }: {
-  perSystem = { pkgs, system, ... }: {
+  perSystem = { pkgs, system, ... }: let
+    phenix-shell = pkgs.writeShellScriptBin "phenix-shell" ''
+      exec ${pkgs.quickshell}/bin/quickshell --config ${toString ../configs/phenix-shell}/shell.qml "$@"
+    '';
+  in {
     packages = {
       hyprland = inputs.hyprland.packages.${system}.hyprland;
 
-      newshell = pkgs.quickshell;
+      inherit phenix-shell;
 
       kitty = pkgs.kitty;
 
