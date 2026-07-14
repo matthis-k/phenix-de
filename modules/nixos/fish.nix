49e7b8ddb6a8a96545b50881bbae6a9b697efeff
Matthis Kälble <69082876+matthis-k@users.noreply.github.com>
2026-07-14 03:05:57 +0200

    fix: group Fish program configuration

--- modules/nixos/fish.nix
+++ modules/nixos/fish.nix
@@ -3,12 +3,14 @@
 {
   imports = [ inputs.nix-index-database.nixosModules.nix-index ];
 
-  programs.fish.enable = true;
-  programs.nix-index = {
-    enable = true;
-    enableFishIntegration = false;
+  programs = {
+    fish.enable = true;
+    nix-index = {
+      enable = true;
+      enableFishIntegration = false;
+    };
+    nix-index-database.comma.enable = true;
   };
-  programs.nix-index-database.comma.enable = true;
 
   users.defaultUserShell = pkgs.fish;
 }
