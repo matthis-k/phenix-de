9b101c9e02eaa489512452dc2a84fe5f18f184ba
Matthis Kälble <69082876+matthis-k@users.noreply.github.com>
2026-07-14 02:53:40 +0200

    feat: add system-wide desktop theming

---
+++ modules/nixos/stylix.nix
@@ -0,0 +1,30 @@
+{ inputs }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  theme = import ../lib/theme.nix { inherit lib; };
+  palette = config.phenix.de.theme.palette;
+in
+{
+  imports = [ inputs.stylix.nixosModules.stylix ];
+
+  options.phenix.de.theme.palette = theme.paletteOption;
+
+  config = {
+    programs.dconf.enable = true;
+
+    stylix = theme.mkStylixConfig { inherit pkgs palette; } // {
+      homeManagerIntegration.autoImport = false;
+    };
+
+    home-manager.sharedModules = [
+      {
+        phenix.de.theme.palette = lib.mkDefault palette;
+      }
+    ];
+  };
+}
